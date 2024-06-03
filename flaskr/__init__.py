f1da442f1d3e32a4a1e8747d17814f226f3c5173
Nilusha Wimalasena <nw.nilusha@gmail.com>
2024-06-03 11:10:27 +0100

    Add login and signup HTML

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,7 +5,15 @@ def create_app():
 
     @app.route("/")#URL leading to method
     def index():
-       return render_template('index.html')
+       return render_template('login.html')
+    
+    @app.route('/login', methods=['GET', 'POST'])
+    def login():
+        return render_template('index.html')
+    
+    @app.route('/signup', methods=['GET', 'POST'])
+    def signup():
+        return render_template('signup.html')
 
     return app
 
