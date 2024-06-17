6b09e26e579f3a832cff2ca73daba2d4bad9f290
Nilusha Wimalasena <nw.nilusha@gmail.com>
2024-06-17 17:04:37 +0100

    login and signup

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,19 +1,51 @@
-from flask import Flask, render_template
+import secrets
+import string
+from flask import Flask, redirect, render_template, request, url_for
+from .db import get_db
+
+def generate_secret_key(length=32):
+    alphabet = string.ascii_letters + string.digits + '!@#$%^&*()-=_+'
+    return ''.join(secrets.choice(alphabet) for _ in range(length))
 
 def create_app():
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = generate_secret_key()
 
-    @app.route("/")#URL leading to method
-    def index():
-       return render_template('login.html')
-    
-    @app.route('/login', methods=['GET', 'POST'])
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['MYSQL_USER'] = 'dbs'
+    app.config['MYSQL_PASSWORD'] = 'password'
+    app.config['MYSQL_DB'] = 'WDIS'
+
+    @app.route('/', methods=['GET', 'POST'])
     def login():
-        return render_template('index.html')
+       print("Inside login module")
+       if request.method == 'POST':
+            username = request.form['email']
+            password = request.form['password']
+
+            db = get_db()
+            cursor = db.cursor()
+            cursor.execute('SELECT username, password FROM Users WHERE username = %s AND password = %s;', (username,password))
+            user = cursor.fetchone()
+            cursor.close()
+
+            if user:
+                return render_template('home.html')
+            else:
+                msg = 'Incorrect Username or Password'
+                return render_template('login.html', msg=msg)
+       else:
+        msg = 'Incorrect Username or Password'
+        return render_template('signup.html', msg=msg)
     
     @app.route('/signup', methods=['GET', 'POST'])
     def signup():
         return render_template('signup.html')
+    
+    @app.route('/home', methods=['GET', 'POST'])
+    def home():
+        return render_template('home.html')
+    
 
     return app
 
